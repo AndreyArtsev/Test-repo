2011028bc97e02936659259ac09a61985ebac400
Andery Artsev <arcevav@yandex.ru>
2023-05-17 17:49:45 +0500

    modifid test_repo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = 10;
+int[] array = new int[n];
+Random random = new Random();
+
+for (int i = 0; i < n; i++)
+{
+    array[i] = random.Next(0, 100);
+}
+Console.WriteLine("Array is: ");
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine(array[i] + "");
+}
+Console.WriteLine();
+
+Console.WriteLine(array[3]);
